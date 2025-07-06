b8d2ef5a18c0998a61f2fc8cf937e40eb6cb47f6
Suresh E <sureshe@arffy.com>
2025-07-06 17:19:36 +0530

    VIT

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
- 
+
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 5173,       
-    allowedHosts: ['finserv.arffy.com','omfinserv.com','www.omfinserv.com','finserv-1-s11k.onrender.com/']
+    port: 5173,
+    allowedHosts: [
+      'finserv.arffy.com',
+      'omfinserv.com',
+      'www.omfinserv.com',
+      'finserv-1-s11k.onrender.com' 
+    ]
   }
 })
- 
